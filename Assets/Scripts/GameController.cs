b353e0143ffd30762eb48f79b4549efa658d2f1d
rvanderneut1 <r.vanderneut1@avans.nl>
2026-01-13 09:03:25 +0100

    Add project files.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public UIManager ui; 
+
+    
+    private void OnActionButtonClicked()
+    {
+       
+        ui.UpdateLabel("Button geklikt! Tekst is aangepast 🎉");
+    }
+}
